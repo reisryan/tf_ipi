32be58aa4fcba4c5292571bb30065ec53032fc74
Mattelis <mattiastelis@gmail.com>
2026-07-15 13:45:38 -0300

    Terminada avaliação e agrupada em uma planilha excel

--- avaliacao.py
+++ avaliacao.py
@@ -27,10 +27,10 @@ for m in range(1,11):
         dig2 = m % 10
         print("Globalizada: ",m)
         sp.Popen(f'./Avaliacao/DIBCO_metrics.exe ./Avaliacao/GT/H{dig1}{dig2}_estGT.tiff ./resultado_HDIBCO/H{dig1}{dig2}_GPos_Processada.png ./Avaliacao/GT/H{dig1}{dig2}_estGT_RWeights.dat ./Avaliacao/GT/H{dig1}{dig2}_estGT_PWeights.dat')
-        time.sleep(15)
+        time.sleep(30)
         print("Nick: ",m)
         sp.Popen(f'./Avaliacao/DIBCO_metrics.exe ./Avaliacao/GT/H{dig1}{dig2}_estGT.tiff ./resultado_HDIBCO/H{dig1}{dig2}_NPos_Processada.png ./Avaliacao/GT/H{dig1}{dig2}_estGT_RWeights.dat ./Avaliacao/GT/H{dig1}{dig2}_estGT_PWeights.dat')
-        time.sleep(15)
+        time.sleep(30)
         print("\n")
     except:
         print("ERROR")
